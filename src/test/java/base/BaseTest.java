1b58220be65405cf53cd87ef1817d45ef67f596f
Yusuf Akgun <yusufakgun1050@gmail.com>
2025-12-26 21:31:26 +0300

    Refactor setup method to use ChromeOptions for headless execution and improved performance

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -3,6 +3,7 @@ package base;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -10,12 +11,18 @@ public class BaseTest {
 
     protected WebDriver driver;
 
-    @BeforeMethod
-    public void setup() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-    }
+   @BeforeMethod
+public void setup() {
+    WebDriverManager.chromedriver().setup();
+
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--headless=new");
+    options.addArguments("--no-sandbox");
+    options.addArguments("--disable-dev-shm-usage");
+    options.addArguments("--window-size=1920,1080");
+
+    driver = new ChromeDriver(options);
+}
 
     @AfterMethod
     public void tearDown() {
