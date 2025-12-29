cdbe6f99696049ac484a2e83b433ff599070518e
Yusuf Akgun <yusufakgun1050@gmail.com>
2025-12-29 19:08:36 +0300

    Enhance BaseTest to include screenshot functionality on test failure

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,34 +1,50 @@
 package base;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class BaseTest {
 
     protected WebDriver driver;
 
-   @BeforeMethod
-public void setup() {
-    WebDriverManager.chromedriver().setup();
-
-    ChromeOptions options = new ChromeOptions();
-    options.addArguments("--headless=new");
-    options.addArguments("--no-sandbox");
-    options.addArguments("--disable-dev-shm-usage");
-    options.addArguments("--window-size=1920,1080");
-
-    driver = new ChromeDriver(options);
-}
+    @BeforeMethod
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
 
     @AfterMethod
-    public void tearDown() {
+    public void tearDown(ITestResult result) {
+        if (ITestResult.FAILURE == result.getStatus()) {
+            takeScreenshot(result.getName());
+        }
         if (driver != null) {
             driver.quit();
         }
     }
-}
 
+    private void takeScreenshot(String testName) {
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File source = ts.getScreenshotAs(OutputType.FILE);
+
+        File target = new File("target/screenshots/" + testName + ".png");
+
+        try {
+            Files.createDirectories(target.getParentFile().toPath());
+            Files.copy(source.toPath(), target.toPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
