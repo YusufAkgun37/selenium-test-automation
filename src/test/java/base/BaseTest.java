74b98fa04f3671103552ef317d1700398dcde2a9
Yusuf Akgun <yusufakgun1050@gmail.com>
2026-01-13 23:24:16 +0300

    feat: Implement ConfigReader and DriverFactory pattern
    Introduced config.properties for centralized configuration. Added ConfigReader and DriverFactory utilities. Refactored BaseTest to use the new driver initialization logic.

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -7,35 +7,28 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import pages.LoginPage;
+import utils.ConfigReader;
+import utils.DriverFactory;
 
 
 public class BaseTest {
 
     protected WebDriver driver;
+    protected LoginPage loginPage;
+
     public WebDriver getDriver() {
         return driver;
     }
 
-
-    protected LoginPage loginPage;
-
     @BeforeMethod
-public void setup() {
-    WebDriverManager.chromedriver().setup();
+    public void setup() {
+        driver = DriverFactory.createDriver();
+        driver.get(ConfigReader.getProperty("url"));
 
-    ChromeOptions options = new ChromeOptions();
-    options.addArguments("--headless=new");
-    options.addArguments("--disable-gpu");
-    options.addArguments("--window-size=1920,1080");
+        loginPage = new LoginPage(driver);
+    }
 
-    driver = new ChromeDriver(options);
-    driver.manage().window().maximize();
-
-    loginPage = new LoginPage(driver);
-}
-
-
-@AfterMethod
+    @AfterMethod
     public void tearDown() {
         if (driver != null) {
             driver.quit();
---
+++ src/test/java/utils/DriverFactory.java
@@ -0,0 +1,46 @@
+package utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+public class DriverFactory {
+
+    public static WebDriver createDriver() {
+        String browser = ConfigReader.getProperty("browser").toLowerCase();
+        boolean headless = ConfigReader.getBoolean("headless");
+
+        WebDriver driver;
+
+        switch (browser) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                if (headless) {
+                    chromeOptions.addArguments("--headless=new");
+                }
+                chromeOptions.addArguments("--disable-gpu");
+                chromeOptions.addArguments("--window-size=1920,1080");
+                driver = new ChromeDriver(chromeOptions);
+                break;
+
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                if (headless) {
+                    firefoxOptions.addArguments("--headless");
+                }
+                driver = new FirefoxDriver(firefoxOptions);
+                break;
+
+            default:
+                throw new RuntimeException("Browser not supported: " + browser);
+        }
+
+        driver.manage().window().maximize();
+        return driver;
+    }
+}
